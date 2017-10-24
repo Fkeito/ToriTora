48c773f87d9c580a028372436e20fff8afbfb91d
Fkeito <1417089@ed.tus.ac.jp>
2017-10-24 23:58:08 +0900

    家具のインポートと部屋の仮作成

---
+++ Assets/VRProject/_Individual/Keito/pack/_Scripts/CageController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CageController : MonoBehaviour {
+
+    public Animator anim;
+    private float moveTime = 5f;
+    private float time = 0;
+
+    public GameObject key;
+
+	void Start () {
+        anim.SetBool("haveBird", true);
+	}
+	
+	void Update () {
+        time += Time.deltaTime;
+
+        if(time > moveTime)
+        {
+            anim.SetBool("moveFlag", true);
+            time = 0;
+        }
+	}
+
+    void SetGlipped(bool isGlipped)
+    {
+        anim.SetBool("isGlipped", isGlipped);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(key.GetInstanceID() == other.gameObject.GetInstanceID())
+        {
+            Destroy(key);
+            anim.SetBool("haveBird", false);
+            GameObject.Find("Bird").transform.parent = null;
+        }
+    }
+}
---
+++ Assets/VRProject/_Individual/Keito/pack/_Scripts/DoorController.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorController : MonoBehaviour {
+
+    [Range(0, 3)]
+    public int gimmick;
+
+    public GameObject light1;
+    public GameObject light2;
+    public GameObject light3;
+    private Renderer rend1;
+    private Renderer rend2;
+    private Renderer rend3;
+    private bool l1 = false;
+    private bool l2 = false;
+    private bool l3 = false;
+
+    public Material on;
+    public Material off;
+
+    public Animator anim;
+
+	void Start () {
+        rend1 = light1.GetComponent<Renderer>();
+        rend2 = light2.GetComponent<Renderer>();
+        rend3 = light3.GetComponent<Renderer>();
+        rend1.material = off;
+        rend2.material = off;
+        rend3.material = off;
+    }
+	
+	void Update () {
+        ChangeColor(gimmick);
+
+        if(gimmick == 3)
+        {
+            if (!anim.GetBool("clear")) anim.SetBool("clear", true);
+        }
+        else
+        {
+            if (anim.GetBool("clear")) anim.SetBool("clear", false);
+        }
+	}
+
+    void ChangeColor(int n)
+    {
+        switch (n)
+        {
+            case 0:
+                if (l1)
+                {
+                    rend1.material = off;
+                    l1 = false;
+                }
+                break;
+            case 1:
+                if (!l1)
+                {
+                    rend1.material = on;
+                    l1 = true;
+                }
+                if (l2)
+                {
+                    rend2.material = off;
+                    l2 = false;
+                }
+                break;
+            case 2:
+                if (!l2)
+                {
+                    rend2.material = on;
+                    l2 = true;
+                }
+                if (l3)
+                {
+                    rend3.material = off;
+                    l3 = false;
+                }
+                break;
+            case 3:
+                if (!l3)
+                {
+                    rend3.material = on;
+                    l3 = true;
+                }
+                break;
+        }
+    }
+
+    void Pull()
+    {
+        if (anim.GetBool("pulling")) return;
+        anim.SetBool("pulling", true);
+    }
+}
