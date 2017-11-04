8abb7f801b69b85ee7053577c611aa7c1c5ee6bb
ryous <1417065@ed.tus.ac.jp>
2017-11-04 20:40:54 +0900

    KOmi

---
+++ Assets/VRProject/_Individual/Ryous/KeyButton.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyButton : VRObjectBase {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+           
+
+            Ray ray = new Ray();
+            RaycastHit hit = new RaycastHit();
+            //    ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            ray = this.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+
+            //マウスクリックした場所からRayを飛ばし、オブジェクトがあればtrue 
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity))
+            {
+               // Debug.Log("raytobasita");
+              //  Debug.Log(hit.collider.gameObject.tag);
+                if (hit.collider.gameObject.tag == "KeyButton")
+                {
+
+                    Unlock();
+
+                }
+            }
+        }
+    }*/
+    public  void Unlock()
+    {
+       Debug.Log("Unlock");
+    }
+}
---
+++ Assets/VRProject/_Individual/Ryous/Mizu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mizu : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject explosionParticle;
+    public GameObject HUU;
+	void Start () {
+
+      // 爆発パーティクル
+}
+
+// Update is called once per frame
+   private  void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name=="mizuhu-senn")
+        {
+          
+            Destroy(gameObject);
+          //  Destroy(HUU);
+            Instantiate(explosionParticle);        
+            }
+        }
+    }
+
+    
+
