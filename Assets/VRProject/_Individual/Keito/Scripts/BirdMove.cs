93cb0ad3ea8da7f70700286b8aebe79e1c28a323
Moosan <washer2016@outlook.jp>
2017-11-18 09:22:03 +0900

    Ending修正,Start画面追加

--- Assets/VRProject/_Individual/Keito/Scripts/BirdMove.cs
+++ Assets/VRProject/_Individual/Keito/Scripts/BirdMove.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class BirdMove : MonoBehaviour
 {
+    [SerializeField]
+    private MovingMode movingMode;
+
+    [SerializeField]
+    private StopMode stopMode;
+    [SerializeField]
+    private float stopTime;
 
     [SerializeField]
     private Vector3[] movePoint;
 
     private Vector3 targetPoint;
+    private int targetNum = 0;
 
     private float speed = 3;
 
@@ -16,15 +24,21 @@ public class BirdMove : MonoBehaviour
     private bool moveFlag = false;
     private bool fly = false;
     private float time = 0;
-    private float stopTime = 0;
+    private float stoptime = 0;
+
+    private Bird bird;
 
     void Start()
     {
         //Time.timeScale = 2;
 
         if (movePoint.Length == 0) Destroy(this);
+        if (movingMode == MovingMode.NeverMove) Destroy(this);
 
-        anim = this.gameObject.GetComponent<Bird>().anim;
+        bird = this.gameObject.GetComponent<Bird>();
+        anim = bird.anim;
+
+        if (bird.isFly || bird.isFlyBack || anim.GetBool("inCage")) return;
 
         Vector3 tmpPos = Vector3.zero;
         targetPoint = SetTargetPosition();
@@ -53,10 +67,11 @@ public class BirdMove : MonoBehaviour
     void Update()
     {
         time += Time.deltaTime;
+        if (bird.isFly || bird.isFlyBack || anim.GetBool("inCage")) return;
 
         Move();
 
-        if (time > stopTime)
+        if (time > stoptime)
         {
             if (!moveFlag)
             {
@@ -71,7 +86,7 @@ public class BirdMove : MonoBehaviour
                     anim.SetFloat("Speed", speed);
                 }
 
-                
+
                 this.GetComponent<Rigidbody>().useGravity = false;
                 this.GetComponent<Rigidbody>().velocity = Vector3.zero;
                 this.GetComponentsInChildren<Collider>()[1].isTrigger = true;
@@ -84,11 +99,11 @@ public class BirdMove : MonoBehaviour
             if (anim.GetBool("Action")) anim.SetBool("Action", false);
             if (anim.GetBool("isPecking")) anim.SetBool("isPecking", false);
 
-            if (time > 5 && time < 6 && stopTime > 10)
+            if (time > 5 && time < 6 && stoptime > 10)
             {
                 anim.SetBool("Action", true);
             }
-            else if(time > 8 && time < 9 && stopTime > 15)
+            else if (time > 8 && time < 9 && stoptime > 15)
             {
                 anim.SetBool("isPecking", true);
             }
@@ -99,10 +114,10 @@ public class BirdMove : MonoBehaviour
     {
         if (moveFlag)
         {
-
             this.transform.Translate(Vector3.forward * speed * Time.deltaTime);
             Quaternion targetRotation = Quaternion.LookRotation(targetPoint - this.transform.position);
             this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetRotation, Time.deltaTime * 3);
+
         }
         else
         {
@@ -112,7 +127,20 @@ public class BirdMove : MonoBehaviour
 
     Vector3 SetTargetPosition()
     {
-        int tmpNum = Random.Range(0, movePoint.Length);
+        int tmpNum = 0;
+
+        if (movingMode == MovingMode.Random) tmpNum = Random.Range(0, movePoint.Length);
+        else if (movingMode == MovingMode.RightOrder)
+        {
+            if (targetNum == movePoint.Length) Destroy(this);
+            tmpNum = targetNum++;
+        }
+        else if(movingMode == MovingMode.Repetition)
+        {
+            if (targetNum == movePoint.Length) targetNum = 0;
+            tmpNum = targetNum++;
+        }
+
         return movePoint[tmpNum];
     }
 
@@ -123,7 +151,7 @@ public class BirdMove : MonoBehaviour
         this.transform.forward = Vector3.Slerp(this.transform.forward, tmpForward.normalized, Time.deltaTime * 2);
     }
 
-    float Distance(Vector3 a,Vector3 b)
+    float Distance(Vector3 a, Vector3 b)
     {
         float answer = (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y) + (a.z - b.z) * (a.z - b.z);
         return Mathf.Sqrt(answer);
@@ -131,9 +159,9 @@ public class BirdMove : MonoBehaviour
 
     void OnCollisionStay(Collision other)
     {
-        if(this.transform.position.y < other.transform.position.y)
+        if (this.transform.position.y < other.transform.position.y)
         {
-            if(other.gameObject.name == "Plane")
+            if (other.gameObject.name == "Plane")
             {
                 this.transform.position = new Vector3(this.transform.position.x, 0.01f, this.transform.position.z);
             }
@@ -158,7 +186,7 @@ public class BirdMove : MonoBehaviour
 
                 if (tmpPos.y != targetPoint.y) fly = true;
 
-                if(fly || Distance(tmpPos, targetPoint) > 5f)
+                if (fly || Distance(tmpPos, targetPoint) > 5f)
                 {
                     speed = 3;
                 }
@@ -170,9 +198,25 @@ public class BirdMove : MonoBehaviour
                 anim.SetFloat("Speed", 0);
                 anim.SetBool("isFlying", false);
 
-                stopTime = Random.Range(1, 20);
+                if (stopMode == StopMode.Random) stoptime = Random.Range(1, 20);
+                else if (stopMode == StopMode.Fix) stoptime = stopTime;
+                else stoptime = 0;
+
                 time = 0;
             }
         }
     }
+}
+enum MovingMode
+{
+    Random,
+    RightOrder,
+    Repetition,
+    NeverMove
+}
+enum StopMode
+{
+    Random,
+    Fix,
+    Zero
 }
---
+++ Assets/VRProject/_Individual/Keito/StartScene/Scripts/Balloon.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Balloon : MonoBehaviour {
+
+    [SerializeField]
+    private float force;
+
+    private Rigidbody rb;
+    private float startPos;
+
+	void Start () {
+        rb = this.GetComponent<Rigidbody>();
+        startPos = this.transform.position.y;
+	}
+	
+	void Update () {
+        if(this.transform.position.y < startPos) rb.AddForce(Vector3.up * force);
+	}
+}
