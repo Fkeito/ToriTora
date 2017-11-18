2c6faa82c9e74628a298ca420827b386586553a1
Moosan <washer2016@outlook.jp>
2017-11-18 15:46:42 +0900

    startadd

---
+++ Assets/schenechanger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class schenechanger : MonoBehaviour {
+    public bool change;
+    void Update(){
+        if (change) {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
